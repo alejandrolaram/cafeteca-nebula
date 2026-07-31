e71d5c1420d16efd8155d31c7d43703cb5a1d552
Alex <alejandrolaram.9@gmail.com>
2026-07-31 12:03:12 -0600

    fix: Configurar propiedades H2 explicitas en CafetecaNebulaApplicationTests

--- src/test/java/com/cafeteca/nebula/CafetecaNebulaApplicationTests.java
+++ src/test/java/com/cafeteca/nebula/CafetecaNebulaApplicationTests.java
@@ -2,8 +2,17 @@ package com.cafeteca.nebula;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class CafetecaNebulaApplicationTests {
 
     @Test
