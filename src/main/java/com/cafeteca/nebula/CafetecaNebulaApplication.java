17f1953131a61bf850c262f6531123741f7419f7
Alex <alejandrolaram.9@gmail.com>
2026-07-31 07:43:09 -0600

    feat: Inicializacion del proyecto para Cafeteca Nebula

---
+++ src/main/java/com/cafeteca/nebula/CafetecaNebulaApplication.java
@@ -0,0 +1,13 @@
+package com.cafeteca.nebula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafetecaNebulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafetecaNebulaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cafeteca/nebula/CafetecaNebulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeteca.nebula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafetecaNebulaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
